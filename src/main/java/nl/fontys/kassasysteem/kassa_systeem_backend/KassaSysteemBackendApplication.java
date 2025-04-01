93a70fc828fd106c79ec18bef955cb6f9e0ef858
Piter Zakko <i475005@fhict.nl>
2025-04-01 15:42:20 +0200

    Eerste commit - koppeling met GitLab repo

---
+++ src/main/java/nl/fontys/kassasysteem/kassa_systeem_backend/KassaSysteemBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.kassasysteem.kassa_systeem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KassaSysteemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KassaSysteemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/fontys/kassasysteem/kassa_systeem_backend/KassaSysteemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.kassasysteem.kassa_systeem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KassaSysteemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
